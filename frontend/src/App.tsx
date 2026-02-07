bf7c4dc4505377ff5bec8f4207e9c2f7afe9ad87
JustAnotherDevv <daniel_zarzecki@wp.pl>
2026-02-07 15:08:18 +0000

    wagmi and wallet connecting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6">
       <h1 className="text-4xl font-bold">Neobank</h1>
+      <ConnectButton />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
